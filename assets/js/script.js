cdb8d1436734b4815ca5ea83f0dbc400ab432770
Amalie Von Behren <vbamalie@gmail.com>
2022-04-01 17:48:14 -0400

    basic html and dom traversal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,46 @@
-console.log("hi")
+//dom traversal variables
+var highscore = document.querySelector(".highscore");//working
+var timer = document.querySelector(".timer");//working
+var question = document.querySelector(".question");
+var multipleChoice = document.querySelector(".multipleChoice");//working
+//not sure if this startQuiz is working
+var startQuiz = document.querySelector(".startQuiz");
+
+
+
+//need a start quiz function
+//start quiz starts a timer
+//delete the html for the text already in the html section
+//start a function for asking the first question 
+//then the second question
+//etc.
+
+//check time function
+//checks to see if they got the previous question right or wrong
+//if wrong lowers time
+//then it checks to make sure the user still has time
+//if they don't have time make variable game over
+
+//first question function. has to change the html header into a question
+//add a list of todos?
+//conditional on whether you selected the correct answer
+//if incorrect, timer decreases
+//returns with a timer decrease and goes to next question
+// or if correct just goes to the next question
+
+
+//second questions works similar as first question
+
+
+
+//start quiz function
+//check time
+//first question
+//check time function
+//second question
+//etc.
+//last question
+//timer amount = high schore
+//locally save high score
+//get the save and attatch to view high score class
+
