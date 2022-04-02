63de9bec86cbf4a1c48124407542ac65e9700140
Amalie Von Behren <vbamalie@gmail.com>
2022-04-02 09:39:22 -0400

    wrote out all the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,76 @@
 //dom traversal variables
 var highscore = document.querySelector(".highscore");
 var timerEl = document.querySelector(".timer");
-var question = document.querySelector(".question");
+var questionText = document.querySelector(".question");
 var multipleChoice = document.querySelector(".multiple-choice");
 var answerEval = document.querySelector(".answer-eval")
-//not sure if this startQuiz is working
-var startQuiz = document.querySelector(".start-quiz");
+var quizButton = document.querySelector("#start-quiz");
 
 //GLOBAL VARIABLES
-var timer = 5
+var timer = 75
+var gameOver = false
+const question1 ={
+    question: "Commonly used data types DO NOT include:",
+    optionA: "Strings",
+    optionB: "Booleans",
+    optionC: "Numbers",
+    optionD: "Alerts",
+}
+const question2 ={
+    question: "The condition in an if / else statement is enclosed within ______.",
+    optionA: "Quotes",
+    optionB: "Curly Brackets",
+    optionC: "Parentheses",
+    optionD: "Square Brackets"
+}
+const question3 ={
+    question: "Arrays in JavaScript can be used to store ______.",
+    optionA: "numbers and strings",
+    optionB: "other arrays",
+    optionC: "booleans",
+    optionD: "all of the above"
+}
+const question4 ={
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    optionA: "commas",
+    optionB: "curly brackets",
+    optionC: "quotes",
+    optionD: "parentheses"
+}
+const question5 ={
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    optionA: "JavaScript",
+    optionB: "terminal / bash",
+    optionC: "for loops",
+    optionD: "console.log"
+}
 
 
-//need a start quiz function
 
-//start quiz starts a timer
+//multiple choice maker
+
+
+//start quiz starts a timer DONE
+//check if the game over function works later though
 function setTime() {
-    // Sets interval in variable
     var timerInterval = setInterval(function() {
       timer--;
       timerEl.textContent = timer;
   
       if(timer === 0) {
-        // Stops execution of action at set interval
         clearInterval(timerInterval);
-        // Calls function to create and append image
+        //when the time hit zero the game is over
+        gameOver = true;
+        return gameOver;
       }
     }, 1000);
   }
-
-setTime()
 //delete the html for the text already in the html section
 //start a function for asking the first question 
 //then the second question
 //etc.
 
-//check time function
+//check time function??
 //checks to see if they got the previous question right or wrong
 //if wrong lowers time
 //then it checks to make sure the user still has time
@@ -53,6 +89,15 @@ setTime()
 
 
 //start quiz function
+quizButton.addEventListener("click" , ()=> {
+    setTime();
+    startQuiz()
+});
+
+function startQuiz(){ 
+    questionText.textContent = question1.question
+
+}
 //check time
 //first question
 //check time function
